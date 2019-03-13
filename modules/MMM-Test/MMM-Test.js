aab4bdafbce7d940d1f93b829c8981afdca6aa97
kimchanyoung <emrhs1004@naver.com>
2019-03-13 20:52:50 +0900

    modules1

---
+++ modules/MMM-Test/MMM-Test.js
@@ -0,0 +1,47 @@
+var Test;
+Module.register("MMM-Test", {
+    defaults: {},
+    start: function (){
+        Test = this;
+        this.count = 0
+    },
+  
+  getDom: function() {
+    var element = document.createElement("div")
+    element.className = "myContent"
+    element.innerHTML = "Hello, World! " + this.config.foo
+    var subElement = document.createElement("p")
+    subElement.innerHTML = "Count:" 
+    subElement.id = "COUNT"
+    element.appendChild(subElement)
+    return element
+  },
+  
+  
+  notificationReceived: function(notification, payload, sender) {
+    switch(notification) {
+      case "DOM_OBJECTS_CREATED":
+      
+      var elem = document.getElementById("COUNT")
+      /*
+      elem.addEventListener("click", () => {
+        this.sendNotification('CHANGE_POSITIONS', ...
+    ...
+    },
+    */
+      elem.addEventListener("click", () => {
+        elem.innerHTML = "startt"
+        this.sendNotification('CHANGE_POSITIONS', 
+        modules = {
+          'clock':{
+              visible: 'true',
+              position: 'middle_center',
+          },
+      }
+  )
+      }) 
+      
+        break
+    }
+  },
+})
