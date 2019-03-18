55a5286e0bd028381c347627be7acd28147d3e23
kimchanyoung <emrhs1004@naver.com>
2019-03-18 16:34:26 +0900

    correct overlapping

--- modules/MMM-Modulebar/MMM-Modulebar.js
+++ modules/MMM-Modulebar/MMM-Modulebar.js
@@ -92,6 +92,17 @@ Module.register("MMM-Modulebar",{
 								fetch(data.showUrl);
 								console.log("Visiting show URL: "+data.showUrl);
 							}
+							//한 프레임에 두가지이상의 모듈이 뜨지 않게 하기.
+							if (modules[i].name == 'MMM-EmbedYoutube') {
+                                console.log("Hiding opend "+ modules[i-1].name+" ID: "+idnr[1]);
+                                modules[i-1].hide(self.config.animationSpeed, {force: self.config.allowForce});
+                                
+                            }
+							else if(modules[i].name == 'MMM-iFrame') {
+                                console.log("Hiding opend "+ modules[i+1].name+" ID: "+idnr[1]);
+                                modules[i+1].hide(self.config.animationSpeed, {force: self.config.allowForce});
+                                
+                            }
 							// 모듈 보이기
 							modules[i].show(self.config.animationSpeed, {force: self.config.allowForce}); 
 							console.log("Showing "+modules[i].name+" ID: "+idnr[1]);
